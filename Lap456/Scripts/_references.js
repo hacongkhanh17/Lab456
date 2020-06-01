fd766985133c892f907f6cb8ce3f3c2aa554bc4d
Huy <conghuy5324@gmail.com>
2020-06-01 08:48:34 +0700

    Tạo một project

--- Lap456/Scripts/_references.js
+++ Lap456/Scripts/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="ai.0.22.9-build00167.min.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
